1ad63649dc32b644b6b1e4d099c8a3b4b53ca3f6
Pesche007 <peter.windemann@gmx.ch>
2014-12-24 09:12:03 +0100

    Report: Legend, pie series watcher
    - Added Legend attribute
    - Added Watcher for pie series change

--- report/time.report.js
+++ report/time.report.js
@@ -36,7 +36,8 @@ angular.module('time')
 			}	
 		else return '<div '+ngClass+' '+popover+' class="'+c+'" style="background:'+bg+';z-Index:'+z+';'+pieRot(ds)+'"><p style="'+pieRot(-ds)+'">'+v+'</p></div>';
 		}
-	var getTemplate=function(data, type, orientation, option){	
+	var getTemplate=function(scope){
+		var data=scope.data, type=scope.type, orientation=scope.orientation, option=scope.option, legend=scope.legend;
 		var html='<div class="arbalochart">';		
 		var MaxNr=0;var valTmp=0;var newData=[];		
 		var num = data.series[0].values.length;
@@ -63,7 +64,7 @@ angular.module('time')
 			var html1st='<div class="pie1half"><div class="pie1">';									
 			var html2nd='<div class="pie2half"><div class="pie2">';			
 			angular.forEach(data.series, function(value, key) {
-				if(key==1) {
+				if(scope.pieSel==value.name) {
 					var count=0, degree=0, degreesum=0, zIndex=1000;
 					for (var i=num; i--;) {
 						count+=value.values[i];
@@ -84,9 +85,6 @@ angular.module('time')
 						degreesum+=degree;
 					   }						
 					}
-				else{
-					
-					}
 				});				
 			if(option=="dougnut") html1st+='<div class="piedougnut1"></div>';										
 			html1st+='</div></div>';
@@ -94,6 +92,7 @@ angular.module('time')
 			html2nd+="</div></div>";						
 			html+=html1st+html2nd;
 			html+='</div>';
+			html+='<select ng-model="pieSel" ng-options="item.name as item.name for item in data.series"></select>';
 			}	
 		if(type=="bar") {
 			if(orientation=="horizontal") html+='<div class="floatBars">';
@@ -141,30 +140,35 @@ angular.module('time')
 					});
 				}
 			}
+		if(type!=="pie" && legend=="true"){
+			html+='<ul class="legend"><li ng-repeat="serie in data.series"><p ng-style="{background: serie.color }"></p>{{serie.name}}</li></ul>';
+			}
 		html+='</div>';	
 		return html;
 		}
   return {
     restrict: 'A',
-	replace:true,
-    scope: {
-      type: '@', orientation: '@', option: '@', data: '='
-    },	
+    scope: {type: '@', orientation: '@', option: '@', legend: '@', data: '='},	
 	controller:function($scope){
 		$scope.changePieColor=function(test, bg){
 			if(test) $scope.PieColor = {background: '#069'};
 			else $scope.PieColor = {background: bg};
 			}
+		$scope.pieSel=$scope.data.series[0].name;
 		},
 	link: function(scope, element, iAttrs) {
-		var htmlText = getTemplate(scope.data, scope.type, scope.orientation, scope.option);
+		var htmlText = getTemplate(scope);
 		var template = angular.element($compile(htmlText)(scope));
-		element.replaceWith(template);		
-		// scope.$watch('arbaloChartData', function(newValue){
-		//	iElement.html(getTemplate(newValue, scope.arbaloChartType));
-		//   });		
-    }
-  }
+		element.html(template);		
+		if(iAttrs.type=="pie") {
+			scope.$watch('pieSel', function(newValue){
+				var htmlText = getTemplate(scope);
+				var template = angular.element($compile(htmlText)(scope));
+				element.html(template);	
+			   });
+			}
+   	 	}
+  	}
 })
   .controller('TimeReportCtrl', function ($scope, $sce, $compile, AppConfig) {
 	AppConfig.setCurrentApp('Time', 'fa-tumblr', 'time', 'app/time/menu.html');
@@ -175,100 +179,25 @@ angular.module('time')
 		"series":[{
 			"name":"Serie 1",
 			"label":["a", "b", "c", "d"],
-			"values":[18,20,2,4],	
+			"values":[18,20,8,14],	
 			"color":"#0099FF"
 			},
 			{
 			"name":"Serie 2",
 			"label":["a1", "b1", "c1", "d1"],
-			"values":[2,2,24,2],	
+			"values":[8,12,24,5],	
 			"color":"#0066FF"
 			},
 			{
 			"name":"Serie 3",
 			"label":["a2", "b2", "c2", "d2"],
-			"values":[4,6,10,10],	
+			"values":[5,6,10,10],	
 			"color":"#0033FF"
 			}],
 		"MaxNr":0,
 		"MaxStackNr":0
 		}
-	//Regular Bar		
-	$scope.myStyleChart = function(data, index, parent){
-		if(data.MaxNr==0) {
-			angular.forEach(data.series, function(value, key) {
-				var tmpMax=Math.max.apply(null,value.values);
-				if(tmpMax > data.MaxNr) data.MaxNr = tmpMax;
-				});			
-			}	
-		var num = data.series[0].values.length;
-		var eWidth=(100/num)/data.series.length;
-		var thisHeight=Math.floor(data.series[parent].values[index]/data.MaxNr*100);				
-		var posL = (data.series.length*index*eWidth)+(parent*eWidth)
-		return {
-			"width" : eWidth+"%",
-			"max-height":thisHeight+"%",
-			"height":thisHeight+"%",
-			"left": posL+"%",
-			"background":data.series[parent].color
-			}
-		}
-	//Regular Bar Lines	
-	$scope.myStyleChartLine = function(data, index, parent){
-		if(data.MaxNr==0) {
-			angular.forEach(data.series, function(value, key) {
-				var tmpMax=Math.max.apply(null,value.values);
-				if(tmpMax > data.MaxNr) data.MaxNr = tmpMax;
-				});			
-			}	
-		var num = data.series[0].values.length;
-		var eWidth=(100/num)/data.series.length;
-		var thisHeight=Math.floor(data.series[parent].values[index]/data.MaxNr*100);				
-		var posL = (data.series.length*index*eWidth)+(parent*eWidth)
-		return {
-			"width" : thisHeight+"%",
-			"background":data.series[parent].color	
-			}
-		}		
-	//Bar stacked
-	$scope.myStyleChartStack = function(data, index, parent){
-		if(data.MaxStackNr==0) {
-			var maxTmp=0;
-			var valTmp=0;
-			for(var i=0;i<data.series[0].values.length;i++){
-				for(var j=0;j<data.series.length;j++){
-					valTmp+=data.series[j].values[i];
-					}
-				if(valTmp>maxTmp) maxTmp=valTmp;
-				valTmp=0;
-				}
-			data.MaxStackNr=maxTmp;	
-			}	
-		var num = data.series[0].values.length;
-		var eWidth=(100/num);
-		var thisHeight=Math.floor(data.series[parent].values[index]/data.MaxStackNr*100);				
-		var posL = (index*eWidth)
-		var posB = 0;
-		if(parent>0) {
-			for(var i=parent-1;i>=0;i--){
-				posB+=Math.floor(data.series[i].values[index]/data.MaxStackNr*100);
-				}			
-			}
-		return {
-			"width" : eWidth+"%",
-			"height":thisHeight+"%",
-			"left": posL+"%",
-			"bottom" : posB+"%",
-			"background":data.series[parent].color	
-			}
-		}				
-	$scope.myStyleChartBottom = function(data){
-		var num = data.length;
-		var thisWidth = Math.floor(100/num);
-		return {
-			"width" : thisWidth+"%",
-			}
-		}
+		
 	$scope.test = function(){
 		$scope.barchart={
 			"x":["Dies", "Das", "Dort", "Da"],
@@ -285,52 +214,4 @@ angular.module('time')
 			"MaxStackNr":0
 			}		
 		}
-	$scope.drawchart = function(){
-		var html='<div class="arbalochart">';
-		var regular=true;
-		var bar=true;
-		var MaxNr=0;		
-		var num = $scope.barchart.series[0].values.length;
-		var seriesnum=$scope.barchart.series.length;
-		var newData=[];
-		var valTmp=0;
-		for(var i=0;i<$scope.barchart.series[0].values.length;i++){
-			var newArr=[];
-			for(var j=0;j<seriesnum;j++){
-				var tmp = $scope.barchart.series[j].values[i];
-				var label = $scope.barchart.series[j].label[i];
-				var col = $scope.barchart.series[j].color;
-				valTmp+=tmp
-				newArr.push([tmp, label, col]);
-				}
-			if(!regular) if(valTmp>MaxNr) MaxNr=valTmp;
-			valTmp=0;
-			newData.push(newArr)
-			}						
-		if(regular) {
-			var eWidth=(100/num)/seriesnum;
-			angular.forEach($scope.barchart.series, function(value, key) {
-				var tmpMax=Math.max.apply(null,value.values);
-				if(tmpMax > MaxNr) MaxNr = tmpMax;
-				});			
-			}
-		else var eWidth=25;
-		html+='<div class="floatBars">';
-		angular.forEach(newData, function(value, key) {
-			for(var i=0;i<value.length;i++){
-				var thisHeight=Math.floor(value[i][0]/MaxNr*100);
-				html+='<div class="bars" popover="'+value[i][1]+'" popover-placement="top" popover-trigger="mouseenter" style="height:'+thisHeight+'%;width:'+eWidth+'%;background:'+value[i][2]+'">'+value[i][0]+'</div>';
-				}
-			});		
-		html+='</div>';
-		//return false;
-		//if(bar) var eWidth=(100/num)/$scope.barchart.series.length;
-		//else eWidth=25;			
-		//var posL = (data.series.length*index*eWidth)+(parent*eWidth)
-		html+='</div>';	
-		return html;
-		//$scope.testen = $compile($sce.trustAsHtml(html))($scope);
-		//console.log($scope.testen)
-		}
-	//$scope.drawchart();
   })
